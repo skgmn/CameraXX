605de51318ed69ee91fdd621ff3587a7bec90054
skgmn <suckgamony@gmail.com>
2021-08-31 21:49:07 +0900

    fix crash

--- library/src/main/java/com/github/skgmn/cameraxx/ListenableFutureExtensions.kt
+++ library/src/main/java/com/github/skgmn/cameraxx/ListenableFutureExtensions.kt
@@ -2,7 +2,9 @@ package com.github.skgmn.cameraxx
 
 import com.google.common.util.concurrent.ListenableFuture
 import kotlinx.coroutines.suspendCancellableCoroutine
+import java.util.concurrent.ExecutionException
 import kotlin.coroutines.resume
+import kotlin.coroutines.resumeWithException
 import kotlin.coroutines.suspendCoroutine
 
 internal suspend fun <V> ListenableFuture<V>.await(cancellable: Boolean = true): V {
@@ -10,7 +12,11 @@ internal suspend fun <V> ListenableFuture<V>.await(cancellable: Boolean = true):
         return suspendCancellableCoroutine { cont ->
             val listener = {
                 if (!isCancelled) {
-                    cont.resume(get())
+                    try {
+                        cont.resume(get())
+                    } catch (e: Throwable) {
+                        cont.resumeWithException(unwrapException(e))
+                    }
                 }
             }
             addListener(listener, ImmediateExecutor())
@@ -22,10 +28,22 @@ internal suspend fun <V> ListenableFuture<V>.await(cancellable: Boolean = true):
         return suspendCoroutine { cont ->
             val listener = {
                 if (!isCancelled) {
-                    cont.resume(get())
+                    try {
+                        cont.resume(get())
+                    } catch (e: Throwable) {
+                        cont.resumeWithException(unwrapException(e))
+                    }
                 }
             }
             addListener(listener, ImmediateExecutor())
         }
+    }
+}
+
+private fun unwrapException(e: Throwable): Throwable {
+    return if (e is ExecutionException) {
+        e.cause ?: e
+    } else {
+        e
     }
 }
