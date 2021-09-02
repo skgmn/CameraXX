88200d605648058d5a0b40318c63794d2a4bbf1b
skgmn <suckgamony@gmail.com>
2021-09-02 19:40:07 +0900

    let FocusMeteringState receive FocusMeteringParameters as a constructor parameter

--- library-composable/src/main/java/com/github/skgmn/cameraxx/FocusMeteringState.kt
+++ library-composable/src/main/java/com/github/skgmn/cameraxx/FocusMeteringState.kt
@@ -4,10 +4,12 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.setValue
 
-class FocusMeteringState {
+class FocusMeteringState(
+    parameters: FocusMeteringParameters = FocusMeteringParameters()
+) {
     internal val progressState = mutableStateOf(FocusMeteringProgress.Idle)
 
-    var parameters by mutableStateOf(FocusMeteringParameters())
+    var parameters by mutableStateOf(parameters)
     var meteringPoints by mutableStateOf<MeteringPoints>(TapMeteringPoints())
     val progress by progressState
 }
