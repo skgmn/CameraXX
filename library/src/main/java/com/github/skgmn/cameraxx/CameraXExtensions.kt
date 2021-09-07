e8ec731855c19ec84fb27f4270a1f7fcbfa4e820
skgmn <suckgamony@gmail.com>
2021-09-07 21:48:42 +0900

    change of ImageAnalysis.analyze()
    - do not make SharedFlow any more
    - automatically close ImageProxy

--- library/src/main/java/com/github/skgmn/cameraxx/CameraXExtensions.kt
+++ library/src/main/java/com/github/skgmn/cameraxx/CameraXExtensions.kt
@@ -1,7 +1,6 @@
 package com.github.skgmn.cameraxx
 
 import android.content.Context
-import androidx.annotation.GuardedBy
 import androidx.camera.core.ImageAnalysis
 import androidx.camera.core.ImageCapture
 import androidx.camera.core.ImageCaptureException
@@ -10,23 +9,15 @@ import androidx.camera.lifecycle.ProcessCameraProvider
 import androidx.camera.view.PreviewView
 import com.google.common.util.concurrent.Futures
 import com.google.common.util.concurrent.MoreExecutors
-import kotlinx.coroutines.DelicateCoroutinesApi
 import kotlinx.coroutines.ExperimentalCoroutinesApi
-import kotlinx.coroutines.GlobalScope
 import kotlinx.coroutines.channels.awaitClose
 import kotlinx.coroutines.flow.Flow
-import kotlinx.coroutines.flow.SharingStarted
 import kotlinx.coroutines.flow.callbackFlow
-import kotlinx.coroutines.flow.shareIn
 import kotlinx.coroutines.guava.await
 import java.lang.ref.WeakReference
-import java.util.*
 import kotlin.coroutines.resume
 import kotlin.coroutines.resumeWithException
 import kotlin.coroutines.suspendCoroutine
-
-@GuardedBy("analyzerFlows")
-private val analyzerFlows = WeakHashMap<ImageAnalysis, WeakReference<Flow<ImageProxy>>>()
 
 @Suppress("UnstableApiUsage")
 suspend fun Context.getProcessCameraProvider(): ProcessCameraProvider {
@@ -40,15 +31,17 @@ fun PreviewView.listenPreviewStreamState(): Flow<PreviewView.StreamState> {
 
 suspend fun ImageCapture.takePicture(): ImageProxy {
     return suspendCoroutine { cont ->
-        takePicture(MoreExecutors.directExecutor(), object : ImageCapture.OnImageCapturedCallback() {
-            override fun onCaptureSuccess(image: ImageProxy) {
-                cont.resume(image)
-            }
+        takePicture(
+            MoreExecutors.directExecutor(),
+            object : ImageCapture.OnImageCapturedCallback() {
+                override fun onCaptureSuccess(image: ImageProxy) {
+                    cont.resume(image)
+                }
 
-            override fun onError(exception: ImageCaptureException) {
-                cont.resumeWithException(exception)
-            }
-        })
+                override fun onError(exception: ImageCaptureException) {
+                    cont.resumeWithException(exception)
+                }
+            })
     }
 }
 
@@ -71,21 +64,22 @@ suspend fun ImageCapture.takePicture(
     }
 }
 
-@OptIn(
-    ExperimentalCoroutinesApi::class,
-    DelicateCoroutinesApi::class
-)
+@OptIn(ExperimentalCoroutinesApi::class)
 fun ImageAnalysis.analyze(): Flow<ImageProxy> {
-    return synchronized(analyzerFlows) {
-        analyzerFlows[this]?.get() ?: callbackFlow {
-            setAnalyzer(MoreExecutors.directExecutor(), {
-                trySend(it)
-            })
-            awaitClose {
-                clearAnalyzer()
+    return callbackFlow {
+        val imageProxies = mutableListOf<WeakReference<ImageProxy>>()
+        setAnalyzer(MoreExecutors.directExecutor(), { imageProxy ->
+            if (trySend(imageProxy).isSuccess) {
+                imageProxies.removeAll { it.get() == null }
+                imageProxies += WeakReference(imageProxy)
+            } else {
+                imageProxy.close()
             }
-        }.shareIn(GlobalScope, SharingStarted.WhileSubscribed(), 0).also {
-            analyzerFlows[this] = WeakReference(it)
+        })
+        awaitClose {
+            imageProxies.forEach { it.get()?.close() }
+            imageProxies.clear()
+            clearAnalyzer()
         }
     }
 }
